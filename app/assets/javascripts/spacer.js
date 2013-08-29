a495a5e7670199af6b909b149283cefbfed65335
Robbie Marcelo <rbmrclo@hotmail.com>
2013-08-29 15:24:52 +0800

    Added domready.js

--- app/assets/javascripts/spacer.js
+++ app/assets/javascripts/spacer.js
@@ -1,8 +1,8 @@
-(function () { 
+DomReady.ready(function() {
   var spacers = document.querySelectorAll("[class^='vspacer']");
 
   for (var x = 0; x < spacers.length; x++ ){
     var spacer_height = spacers[x].className.split('-')[1];
     spacers[x].style.height = spacer_height + "px";
   }
-})();
+});
